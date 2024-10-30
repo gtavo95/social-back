fa161b6919298b5577f6de84ddd02e0f58d38644
Gustavo <gustavo.gordillo@mba2021.incae.edu>
2024-10-30 00:44:13 -0600

    se quita el dot env

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	"log"
 	"social/routes"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{DisablePreParseMultipartForm: true, StreamRequestBody: true})
 
 	// Load the .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	app.Use(logger.New())
 	app.Use(recover.New())
 	app.Use(cors.New())
