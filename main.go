ba3f5aacf7faabec237f28a783c5c19cd40ac63c
Gustavo <gustavo.gordillo@mba2021.incae.edu>
2024-10-30 00:13:27 -0600

    update go envs

--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{DisablePreParseMultipartForm: true, StreamRequestBody: true})
 
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	app.Use(logger.New())
 	app.Use(recover.New())
 	app.Use(cors.New())
